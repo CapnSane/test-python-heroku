3b71e5fabdefd1a312b3d78288d70bd14e15f24a
Leonardo Schneider <lnrdschneider@gmail.com>
2022-01-18 10:36:30 -0300

    testing the schedule function on python

--- script.py
+++ script.py
@@ -1,1 +1,20 @@
-print("it's working!")
+import time
+from threading import Timer
+import sched
+
+# def function():
+#     print("it's working!")
+
+# t = Timer(5, function)
+# t.start()
+
+
+event_schedule = sched.scheduler(time.time, time.sleep)
+
+def do_something():
+    print("it's working!")
+    event_schedule.enter(2, 1, do_something) ## It will repeat after 2 seconds itself
+
+# event_schedule.enter(2, 1, do_something) ## Start running do_something() after 2 seconds
+do_something()
+event_schedule.run()
